9df7a0a8b40ca130fc889937bc4b1eeb1d010095
Paolo Calao <paolo.calao@gmail.com>
2022-09-09 00:25:36 +0200

    Draft user fetch

--- core/user/handler.go
+++ core/user/handler.go
@@ -45,3 +45,36 @@ func HandleCreate(db *sqlx.DB) web.Handler {
 		return Create(ctx, db, user)
 	}
 }
+
+func HandleShow(db *sqlx.DB) web.Handler {
+	return func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
+		userID := web.Param(r, "id")
+
+		// TODO: Add auth checks.
+		// claims, err := auth.GetClaims(ctx)
+		// if err != nil {
+		// 	return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+		// }
+		//
+		// // If you are not an admin and looking to retrieve someone other than yourself.
+		// if !claims.Authorized(auth.RoleAdmin) && claims.Subject != userID {
+		// 	return v1Web.NewRequestError(auth.ErrForbidden, http.StatusForbidden)
+		// }
+
+		user, err := Fetch(ctx, db, userID)
+		if err != nil {
+			// TODO: Improve errors.
+			// switch {
+			// case errors.Is(err, user.ErrInvalidID):
+			// 	return v1Web.NewRequestError(err, http.StatusBadRequest)
+			// case errors.Is(err, user.ErrNotFound):
+			// 	return v1Web.NewRequestError(err, http.StatusNotFound)
+			// default:
+			// 	return fmt.Errorf("ID[%s]: %w", userID, err)
+			// }
+			return fmt.Errorf("ID[%s]: %w", userID, err)
+		}
+
+		return web.Respond(ctx, w, user, http.StatusOK)
+	}
+}
--- core/user/store.go
+++ core/user/store.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/jmoiron/sqlx"
+	"github.com/polldo/govod/database"
 )
 
 func Create(ctx context.Context, db sqlx.ExtContext, user User) error {
@@ -14,9 +15,32 @@ func Create(ctx context.Context, db sqlx.ExtContext, user User) error {
 	VALUES
 		(:id, :name, :email, :password, :role, :created_at, :updated_at)`
 
-	if _, err := sqlx.NamedExecContext(ctx, db, q, user); err != nil {
+	if err := database.NamedExecContext(ctx, db, q, user); err != nil {
 		return fmt.Errorf("inserting user: %w", err)
 	}
 
 	return nil
+}
+
+func Fetch(ctx context.Context, db sqlx.ExtContext, id string) (User, error) {
+	in := struct {
+		ID string `db:"id"`
+	}{
+		ID: id,
+	}
+
+	const q = `
+	SELECT
+		*
+	FROM
+		users
+	WHERE 
+		id = :id`
+
+	var user User
+	if err := database.NamedQueryStruct(ctx, db, q, in, &user); err != nil {
+		return User{}, fmt.Errorf("selecting user id[%q]: %w", id, err)
+	}
+
+	return user, nil
 }
