a984fa6250000ef6ac144bad58274fc60fdf42d4
Paolo Calao <paolo.calao@gmail.com>
2022-11-24 12:20:46 +0100

    Test all course handlers

--- api/test/course_test.go
+++ api/test/course_test.go
@@ -3,10 +3,14 @@ package test
 import (
 	"bytes"
 	"encoding/json"
+	"math/rand"
 	"net/http"
+	"strconv"
 	"testing"
 
+	"github.com/ardanlabs/service/business/sys/validate"
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/polldo/govod/core/course"
 )
 
@@ -20,9 +24,19 @@ func TestCourse(t *testing.T) {
 		t.Fatalf("initializing test env: %v", err)
 	}
 
-	ut := &courseTest{env}
-	ut.createCourseOK(t)
-	ut.createCourseUnauth(t)
+	ct := &courseTest{env}
+	c1 := ct.createCourseOK(t)
+	c2 := ct.createCourseOK(t)
+	c2 = ct.updateCourseOK(t, c2)
+	cs := []course.Course{c1, c2}
+
+	ct.createCourseUnauth(t)
+	ct.updateCourseUnauth(t, c2)
+
+	ct.showCourseOK(t, c1)
+	ct.showCourseInvalid(t)
+	ct.showCourseNotFound(t)
+	ct.listCoursesOK(t, cs)
 }
 
 func (ct *courseTest) createCourseOK(t *testing.T) course.Course {
@@ -32,9 +46,9 @@ func (ct *courseTest) createCourseOK(t *testing.T) course.Course {
 	defer Logout(ct.Server)
 
 	c := course.CourseNew{
-		Name:        "Test",
+		Name:        "Test" + strconv.Itoa(rand.Intn(1000)),
 		Description: "This is a test course",
-		Price:       100,
+		Price:       rand.Float64() * 100,
 	}
 
 	body, err := json.Marshal(&c)
@@ -111,7 +125,7 @@ func ptr[T any](a T) *T {
 	return &a
 }
 
-func (ct *courseTest) updateCourseOK(t *testing.T, crs course.Course) {
+func (ct *courseTest) updateCourseOK(t *testing.T, crs course.Course) course.Course {
 	if err := Login(ct.Server, ct.AdminEmail, ct.AdminPass); err != nil {
 		t.Fatal(err)
 	}
@@ -120,7 +134,7 @@ func (ct *courseTest) updateCourseOK(t *testing.T, crs course.Course) {
 	c := course.CourseUp{
 		Name:        ptr("Updated Test"),
 		Description: ptr("This is an updated test course"),
-		Price:       ptr(200.0),
+		Price:       ptr(500.0),
 	}
 
 	body, err := json.Marshal(&c)
@@ -156,6 +170,7 @@ func (ct *courseTest) updateCourseOK(t *testing.T, crs course.Course) {
 	if diff := cmp.Diff(got, exp); diff != "" {
 		t.Fatalf("wrong course payload. Diff: \n%s", diff)
 	}
+	return got
 }
 
 func (ct *courseTest) updateCourseUnauth(t *testing.T, crs course.Course) {
@@ -191,11 +206,93 @@ func (ct *courseTest) updateCourseUnauth(t *testing.T, crs course.Course) {
 	}
 }
 
-func (ct *courseTest) listCoursesOK(t *testing.T, id string) {
+func (ct *courseTest) showCourseOK(t *testing.T, crs course.Course) {
+	r, err := http.NewRequest(http.MethodGet, ct.URL+"/courses/"+crs.ID, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w, err := ct.Client().Do(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer w.Body.Close()
+
+	if w.StatusCode != http.StatusOK {
+		t.Fatalf("can't fetch course: status code %s", w.Status)
+	}
+
+	var got course.Course
+	if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+		t.Fatalf("cannot unmarshal fetched course: %v", err)
+	}
+
+	if diff := cmp.Diff(got, crs); diff != "" {
+		t.Fatalf("wrong course payload. Diff: \n%s", diff)
+	}
 }
 
-func (ct *courseTest) showCourseOK(t *testing.T, id string) {
+func (ct *courseTest) showCourseInvalid(t *testing.T) {
+	invalidID := "SELECT * FROM Users WHERE ((Username='$username') AND (Password=MD5('$password')))"
+
+	r, err := http.NewRequest(http.MethodGet, ct.URL+"/courses/"+invalidID, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w, err := ct.Client().Do(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer w.Body.Close()
+
+	if w.StatusCode == http.StatusOK {
+		t.Fatalf("ill requests should be blocked: status code %s", w.Status)
+	}
 }
 
-func (ct *courseTest) showCourseInvalid(t *testing.T, id string) {
+func (ct *courseTest) showCourseNotFound(t *testing.T) {
+	randomID := validate.GenerateID()
+
+	r, err := http.NewRequest(http.MethodGet, ct.URL+"/courses/"+randomID, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w, err := ct.Client().Do(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer w.Body.Close()
+
+	if w.StatusCode == http.StatusNotFound {
+		t.Fatalf("fetche course should not exist: status code %s", w.Status)
+	}
+}
+
+func (ct *courseTest) listCoursesOK(t *testing.T, crs []course.Course) {
+	r, err := http.NewRequest(http.MethodGet, ct.URL+"/courses", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w, err := ct.Client().Do(r)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer w.Body.Close()
+
+	if w.StatusCode != http.StatusOK {
+		t.Fatalf("can't fetch course: status code %s", w.Status)
+	}
+
+	var got []course.Course
+	if err := json.NewDecoder(w.Body).Decode(&got); err != nil {
+		t.Fatalf("cannot unmarshal fetched courses: %v", err)
+	}
+
+	less := func(a, b course.Course) bool { return a.ID < b.ID }
+	if diff := cmp.Diff(got, crs, cmpopts.SortSlices(less)); diff != "" {
+		t.Fatalf("wrong courses payload. Diff: \n%s", diff)
+	}
 }
