e064b067bd1b46e77e4defe2764d8de5099dcb7a
Paolo Calao <paolo.calao@gmail.com>
2023-02-10 00:08:36 +0100

    Add navbar

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Footer from '@/components/Footer/Footer'
+import Navbar from '@/components/Navbar/Navbar'
 
 export default function Home() {
     return (
@@ -12,6 +13,8 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
+
+            <Navbar />
 
             <main className={styles.main}>
                 <div className={styles.description}></div>
