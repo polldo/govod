97ab1b9f9fbb847ac675fe79af0225807d1e640a
Paolo Calao <paolo.calao@gmail.com>
2023-03-17 00:08:25 +0100

    Revert "Add navbar"
    This reverts commit e064b067bd1b46e77e4defe2764d8de5099dcb7a.

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
 import Footer from '@/components/Footer/Footer'
-import Navbar from '@/components/Navbar/Navbar'
 
 export default function Home() {
     return (
@@ -13,8 +12,6 @@ export default function Home() {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-
-            <Navbar />
 
             <main className={styles.main}>
                 <div className={styles.description}></div>
