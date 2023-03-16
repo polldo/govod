151a5391e2ba3e0d5f119d18951f0b9b786a88a6
Paolo Calao <paolo.calao@gmail.com>
2023-03-17 00:08:27 +0100

    Revert "Add footer"
    This reverts commit 9c6394ff14fa6308a72ec6b6417b2d8c3aebcd78.

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,6 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import { ChakraProvider } from '@chakra-ui/react'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-    return (
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
-    )
+  return <Component {...pageProps} />;
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,36 +1,32 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import Footer from '@/components/Footer/Footer'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>govod</title>
-                <meta name="description" content="govod - webapp for video on demand" />
-                <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
+  return (
+    <>
+      <Head>
+        <title>govod</title>
+        <meta name="description" content="govod - webapp for video on demand" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div className={styles.description}></div>
 
-            <main className={styles.main}>
-                <div className={styles.description}></div>
+        <div className={styles.center}>
+          <Image
+            className={styles.logo}
+            src="/next.svg"
+            alt="Next.js Logo"
+            width={180}
+            height={37}
+            priority
+          />
+        </div>
 
-                <div className={styles.center}>
-                    <Image
-                        className={styles.logo}
-                        src="/next.svg"
-                        alt="Next.js Logo"
-                        width={180}
-                        height={37}
-                        priority
-                    />
-                </div>
-
-                <div className={styles.grid}></div>
-            </main>
-
-            <Footer />
-        </>
-    )
+        <div className={styles.grid}></div>
+      </main>
+    </>
+  );
 }
