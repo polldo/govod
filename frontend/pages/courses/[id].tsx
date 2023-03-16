a766a46e6433cbce515eba758ef0b1ec99d55baf
Paolo Calao <paolo.calao@gmail.com>
2023-03-17 00:49:48 +0100

    Prettify

---
+++ frontend/pages/courses/[id].tsx
@@ -0,0 +1,3 @@
+export default function CoursesID() {
+    return <>oops</>
+}
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,32 +1,32 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "@/styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>govod</title>
-        <meta name="description" content="govod - webapp for video on demand" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <main className={styles.main}>
-        <div className={styles.description}></div>
+    return (
+        <>
+            <Head>
+                <title>govod</title>
+                <meta name="description" content="govod - webapp for video on demand" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <main className={styles.main}>
+                <div className={styles.description}></div>
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-        </div>
+                <div className={styles.center}>
+                    <Image
+                        className={styles.logo}
+                        src="/next.svg"
+                        alt="Next.js Logo"
+                        width={180}
+                        height={37}
+                        priority
+                    />
+                </div>
 
-        <div className={styles.grid}></div>
-      </main>
-    </>
-  );
+                <div className={styles.grid}></div>
+            </main>
+        </>
+    )
 }
