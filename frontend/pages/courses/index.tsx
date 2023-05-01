d1a95724bb89d453d6d789a39f77c41c118fba8f
Paolo Calao <paolo.calao@gmail.com>
2023-05-02 00:01:38 +0200

    Draft courses page (#8)
    * Draft courses page
    
    * Improve course page
    
    * Show courses with props
    
    * Fetch courses from backend
    
    * Change course model according to backend

--- frontend/pages/courses/index.tsx
+++ frontend/pages/courses/index.tsx
@@ -1,9 +1,58 @@
-export default function Courses() {
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+
+type Course = {
+    name: string
+    description: string
+    image: string
+}
+
+const data = [
+    { name: 'ok', description: 'full description', image: '/random.jpeg' },
+    { name: 'second', description: 'second description', image: '/random.jpeg' },
+    { name: 'third', description: 'third description', image: '/random.jpeg' },
+]
+
+function Card(props: Course) {
     return (
-        <>
-            <h1 className="text-3xl font-bold text-red-700 underline">Hello world!</h1>
-            <h1 className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md">Hello world!</h1>
-            oops
-        </>
+        <a
+            href="#"
+            className="flex w-1/2 flex-col items-center rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:max-w-xl md:flex-row"
+        >
+            <Image
+                className="w-full rounded-t-lg border border-red-800 object-contain md:w-20"
+                alt=""
+                src={props.image}
+                width={80}
+                height={32}
+            />
+
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.name}</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.description}</p>
+            </div>
+        </a>
+    )
+}
+
+export default function Courses() {
+    const [courses, setCourses] = useState<Course[]>(data)
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:8080/courses')
+            .then((res) => res.json())
+            .then((data) => setCourses(data))
+    }, [])
+
+    if (!courses) {
+        return null
+    }
+
+    return (
+        <div className="flex flex-col items-center space-y-5 pt-6 pb-6">
+            {courses.map((course) => (
+                <Card {...course} key={course.name} />
+            ))}
+        </div>
     )
 }
