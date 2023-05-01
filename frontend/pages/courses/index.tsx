2a5d6d94bb5635c5650c08d2726cca049d4bcb42
Paolo Calao <paolo.calao@gmail.com>
2023-05-01 23:56:38 +0200

    Frontend startup (#7)
    * Bootstrap frontend app
    
    * Format and clean
    
    * Update frontend dependencies
    
    * Add footer
    
    * Add navbar
    
    * Revert "Add navbar"
    
    This reverts commit e064b067bd1b46e77e4defe2764d8de5099dcb7a.
    
    * Revert "Add footer"
    
    This reverts commit 9c6394ff14fa6308a72ec6b6417b2d8c3aebcd78.
    
    * Revert "Update frontend dependencies"
    
    This reverts commit 43b767c667be5b17d0bb5ead7a746369e4ba12e0.
    
    * Add tailwind dependency
    
    * Add prettier config
    
    * Prettify
    
    * Add prettier as dependency

---
+++ frontend/pages/courses/index.tsx
@@ -0,0 +1,9 @@
+export default function Courses() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold text-red-700 underline">Hello world!</h1>
+            <h1 className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md">Hello world!</h1>
+            oops
+        </>
+    )
+}
