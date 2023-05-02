8c0900b23cb0e5f36120406bb14ef9f2d6a293a9
Paolo Calao <paolo.calao@gmail.com>
2023-05-02 15:06:49 +0200

    Move navbar in layout

--- frontend/components/layout.tsx
+++ frontend/components/layout.tsx
@@ -3,11 +3,33 @@ import type { PropsWithChildren } from 'react'
 export default function Layout(props: PropsWithChildren) {
     return (
         <>
+            <Navbar />
             <main className="overflow-none flex h-screen justify-center">
-                <div className="flex h-full w-full flex-col border-x border-slate-400 md:max-w-2xl">
-                    {props.children}
-                </div>
+                <div className="flex h-full w-full flex-col md:max-w-2xl">{props.children}</div>
             </main>
         </>
+    )
+}
+
+function Navbar() {
+    return (
+        <nav className="bg-gray-900 py-4">
+            <div className="container mx-auto">
+                <div className="flex justify-between">
+                    <div className="text-xl font-bold text-white">My Website</div>
+                    <div>
+                        <a className="rounded px-3 py-2 text-gray-400 hover:text-white" href="#">
+                            Home
+                        </a>
+                        <a className="rounded px-3 py-2 text-gray-400 hover:text-white" href="#">
+                            About
+                        </a>
+                        <a className="rounded px-3 py-2 text-gray-400 hover:text-white" href="#">
+                            Contact
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </nav>
     )
 }
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -8,7 +8,6 @@ export default function Home() {
                 <Head>
                     <title>Govod</title>
                 </Head>
-                <Navbar />
                 <Hero />
             </Layout>
         </>
@@ -31,28 +30,5 @@ function Hero() {
                 </div>
             </div>
         </div>
-    )
-}
-
-function Navbar() {
-    return (
-        <nav className="bg-gray-900 py-4">
-            <div className="container mx-auto">
-                <div className="flex justify-between">
-                    <div className="text-xl font-bold text-white">My Website</div>
-                    <div>
-                        <a className="rounded px-3 py-2 text-gray-400 hover:text-white" href="#">
-                            Home
-                        </a>
-                        <a className="rounded px-3 py-2 text-gray-400 hover:text-white" href="#">
-                            About
-                        </a>
-                        <a className="rounded px-3 py-2 text-gray-400 hover:text-white" href="#">
-                            Contact
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </nav>
     )
 }
