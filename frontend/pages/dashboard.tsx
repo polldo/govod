c1bfe5649025e80d40573be2a9c32f19e27be776
Paolo Calao <paolo.calao@gmail.com>
2023-05-21 22:05:46 +0200

    Add empty dashboard page (#18)

---
+++ frontend/pages/dashboard.tsx
@@ -0,0 +1,31 @@
+import Layout from '@/components/layout'
+import Head from 'next/head'
+import { useSession } from '@/session/context'
+import { useRouter } from 'next/router'
+
+export default function Dashboard() {
+    const router = useRouter()
+    const { isLoggedIn, isLoading } = useSession()
+
+    if (isLoading) {
+        return null
+    }
+
+    if (!isLoggedIn) {
+        router.push('login')
+        return null
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Dashboard</title>
+            </Head>
+            <Layout>
+                <div>
+                    <p>Hello, this is your dashboard!</p>
+                </div>
+            </Layout>
+        </>
+    )
+}
