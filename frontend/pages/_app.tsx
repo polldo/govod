f4645f05295fa27e05e5de50197583eec6d8d432
Paolo Calao <paolo.calao@gmail.com>
2023-05-24 23:11:29 +0200

    Install react toast

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,12 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from '@/session/context'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <SessionProvider {...pageProps}>
+            <Toaster position="bottom-center" />
             <Component {...pageProps} />
         </SessionProvider>
     )
