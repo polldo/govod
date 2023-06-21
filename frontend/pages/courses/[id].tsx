7b807e3158ff9e1ff582c82ba58be54d5431cc32
Paolo Calao <paolo.calao@gmail.com>
2023-06-22 00:50:14 +0200

    Implement course details page

--- frontend/pages/courses/[id].tsx
+++ frontend/pages/courses/[id].tsx
@@ -1,3 +1,135 @@
-export default function CoursesID() {
-    return <>oops</>
+import Layout from '@/components/layout'
+import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import { useFetch } from '@/services/fetch'
+import { toast } from 'react-hot-toast'
+
+type Course = {
+    name: string
+    description: string
+    image: string
+}
+
+type Video = {
+    name: string
+    description: string
+    free: boolean
+}
+
+export default function CourseDetails() {
+    const [course, setCourse] = useState<Course>()
+    const [videos, setVideos] = useState<Video[]>()
+    const fetch = useFetch()
+    const router = useRouter()
+    const { id } = router.query
+
+    const free: Video[] = videos?.filter((video) => video.free) || []
+
+    useEffect(() => {
+        if (!router.isReady) {
+            return
+        }
+
+        fetch('http://mylocal.com:8000/courses/' + id)
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error()
+                }
+                return res.json()
+            })
+            .then((data) => setCourse(data))
+            .catch(() => {
+                toast.error('Something went wrong')
+            })
+    }, [id, fetch, router.isReady])
+
+    useEffect(() => {
+        if (!router.isReady) {
+            return
+        }
+
+        fetch('http://mylocal.com:8000/courses/' + id + '/videos')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error()
+                }
+                return res.json()
+            })
+            .then((data) => setVideos(data))
+            .catch(() => {
+                toast.error('Something went wrong')
+            })
+    }, [id, fetch, router.isReady])
+
+    return (
+        <>
+            <Head>
+                <title>Course - {course?.name}</title>
+            </Head>
+            <Layout>
+                <div className="flex w-1/2 flex-col ">
+                    <div className="mx-auto my-20 flex flex-row">
+                        <Image
+                            className="mx-20 rounded-t-lg border border-red-800 object-contain md:w-20"
+                            alt=""
+                            src={''}
+                            width={80}
+                            height={32}
+                        />
+                        <div className="flex flex-col">
+                            <p>{course?.name}</p>
+                            <p>{course?.description}</p>
+                        </div>
+                    </div>
+
+                    {free && free.length > 0 && (
+                        <div className="flex w-full flex-col">
+                            <p className="mx-auto">Free Samples</p>
+                            <div className="flex w-full flex-col items-center space-y-5 pt-6 pb-6">
+                                {free.map((video) => (
+                                    <Card {...video} key={video.name} />
+                                ))}
+                            </div>
+                        </div>
+                    )}
+
+                    <div className="flex w-full flex-col">
+                        <p className="mx-auto">All videos contained in this course</p>
+                        <div className="flex flex-col items-center space-y-5 pt-6 pb-6">
+                            {videos && videos.map((video) => <Card {...video} key={video.name} />)}
+                        </div>
+                    </div>
+                </div>
+            </Layout>
+        </>
+    )
+}
+
+function Card(props: Video) {
+    return (
+        <div className="flex w-1/2 flex-col items-center rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100 md:max-w-xl md:flex-row">
+            <Image
+                className="w-full rounded-t-lg border border-red-800 object-contain md:w-20"
+                alt=""
+                src={''}
+                width={80}
+                height={32}
+            />
+
+            <div className="flex flex-col justify-between p-4 leading-normal">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.name}</h5>
+                <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.description}</p>
+            </div>
+
+            {props.free && (
+                <Link href="#" className="rounded bg-blue-500 p-4 font-semibold text-white">
+                    Play
+                </Link>
+            )}
+        </div>
+    )
 }
--- frontend/pages/courses/index.tsx
+++ frontend/pages/courses/index.tsx
@@ -1,24 +1,20 @@
 import Layout from '@/components/layout'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import { useEffect, useState } from 'react'
 
 type Course = {
+    id: string
     name: string
     description: string
     image: string
 }
 
-const data = [
-    { name: 'ok', description: 'full description', image: '/random.jpeg' },
-    { name: 'second', description: 'second description', image: '/random.jpeg' },
-    { name: 'third', description: 'third description', image: '/random.jpeg' },
-]
-
 function Card(props: Course) {
     return (
-        <a
-            href="#"
+        <Link
+            href={`/courses/${encodeURIComponent(props.id)}`}
             className="flex w-1/2 flex-col items-center rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:max-w-xl md:flex-row"
         >
             <Image
@@ -33,12 +29,12 @@ function Card(props: Course) {
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.name}</h5>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.description}</p>
             </div>
-        </a>
+        </Link>
     )
 }
 
 export default function Courses() {
-    const [courses, setCourses] = useState<Course[]>(data)
+    const [courses, setCourses] = useState<Course[]>()
 
     useEffect(() => {
         fetch('http://mylocal.com:8000/courses')
