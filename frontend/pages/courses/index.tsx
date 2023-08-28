37b6b89bd4cdee3bc13e026f5e04ccf171a82261
Paolo Calao <paolo.calao@gmail.com>
2023-08-29 00:51:49 +0200

    Integrate cart in courses page

--- frontend/pages/courses/index.tsx
+++ frontend/pages/courses/index.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import { useSession } from '@/session/context'
+import { toast } from 'react-hot-toast'
+import { useFetch } from '@/services/fetch'
 
 type Course = {
     id: string
@@ -11,7 +14,21 @@ type Course = {
     image: string
 }
 
-function Card(props: Course) {
+type Cart = {
+    items: CartItem[]
+}
+
+type CartItem = {
+    course_id: string
+}
+
+type CardProps = Course & {
+    isOwned: boolean
+    isInCart: boolean
+    isLoggedIn: boolean
+}
+
+function Card(props: CardProps) {
     return (
         <Link
             href={`/courses/${encodeURIComponent(props.id)}`}
@@ -28,6 +45,13 @@ function Card(props: Course) {
             <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.name}</h5>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.description}</p>
+                {props.isOwned ? (
+                    <button onClick={() => (window.location.href = `/courses/${props.id}`)}>Go to Course</button>
+                ) : props.isInCart ? (
+                    <button disabled>Already in Cart</button>
+                ) : (
+                    <button onClick={() => {}}>{props.isLoggedIn ? 'Add to Cart' : 'Login to Purchase'}</button>
+                )}
             </div>
         </Link>
     )
@@ -35,12 +59,64 @@ function Card(props: Course) {
 
 export default function Courses() {
     const [courses, setCourses] = useState<Course[]>()
+    const [cartCourses, setCartCourses] = useState<string[]>([])
+    const [isLoadingCart, setIsLoadingCart] = useState<boolean>(true)
+    const [ownedCourses, setOwnedCourses] = useState<string[]>([])
+    const [isLoadingOwned, setIsLoadingOwned] = useState<boolean>(true)
+    const { isLoggedIn, isLoading } = useSession()
+    const fetch = useFetch()
 
     useEffect(() => {
         fetch('http://mylocal.com:8000/courses')
             .then((res) => res.json())
             .then((data) => setCourses(data))
-    }, [])
+    }, [fetch])
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            return
+        }
+
+        fetch('http://mylocal.com:8000/cart')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error()
+                }
+                return res.json()
+            })
+            .then((data: Cart) => {
+                const incart = data.items.map((item: CartItem) => item.course_id)
+                setCartCourses(incart)
+                setIsLoadingCart(false)
+            })
+            .catch(() => {
+                toast.error('Something went wrong')
+            })
+
+        fetch('http://mylocal.com:8000/courses/owned')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error()
+                }
+                return res.json()
+            })
+            .then((data: Course[]) => {
+                const owned = data.map((item: Course) => item.id)
+                setOwnedCourses(owned)
+                setIsLoadingOwned(false)
+            })
+            .catch(() => {
+                toast.error('Something went wrong')
+            })
+    }, [fetch, isLoggedIn])
+
+    if (isLoading) {
+        return null
+    }
+
+    if (isLoggedIn && (isLoadingCart || isLoadingOwned)) {
+        return null
+    }
 
     if (!courses) {
         return null
@@ -54,7 +130,13 @@ export default function Courses() {
             <Layout>
                 <div className="flex flex-col items-center space-y-5 pt-6 pb-6">
                     {courses.map((course) => (
-                        <Card {...course} key={course.name} />
+                        <Card
+                            {...course}
+                            isOwned={ownedCourses.includes(course.id)}
+                            isInCart={cartCourses.includes(course.id)}
+                            isLoggedIn={isLoggedIn}
+                            key={course.name}
+                        />
                     ))}
                 </div>
             </Layout>
