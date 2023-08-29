a98bff46a3bdeec780ed787b694892b229759485
Paolo Calao <paolo.calao@gmail.com>
2023-08-30 00:12:02 +0200

    Implement a cart (#26)
    * Draft cart page
    
    * Add delete item
    
    * Integrate cart in courses page
    
    * Add to cart
    
    * Add cart icon to navbar
    
    * Reset owned and cart courses upon logout
    
    * Change link if course is owned
    
    * Move logic around

---
+++ frontend/pages/cart.tsx
@@ -0,0 +1,164 @@
+import Layout from '@/components/layout'
+import Head from 'next/head'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { useSession } from '@/session/context'
+import { useFetch } from '@/services/fetch'
+import Image from 'next/image'
+import { toast } from 'react-hot-toast'
+
+type CartType = {
+    items: Item[]
+}
+
+type Item = {
+    course_id: string
+}
+
+type Course = {
+    id: string
+    name: string
+    price: number
+}
+
+type CoursesMap = {
+    [courseId: string]: Course
+}
+
+type Product = {
+    id: string
+    name: string
+    image: string
+    price: number
+}
+
+type ProductCardProps = {
+    product: Product
+    onDelete: (x: string) => void
+}
+
+function ProductCard(props: ProductCardProps) {
+    return (
+        <div className="mb-2 flex flex-col items-center justify-between rounded bg-white p-4 shadow sm:flex-row">
+            <button
+                onClick={() => props.onDelete(props.product.id)}
+                className="rounded bg-red-500 px-4 py-2 text-white"
+            >
+                x
+            </button>
+            <Image src={props.product.image} alt={props.product.name} className="h-20 w-20" />
+            <div className="ml-4">{props.product.name}</div>
+            <div className="font-bold">${props.product.price}</div>
+        </div>
+    )
+}
+
+export default function Cart() {
+    const [cart, setCart] = useState<CartType>()
+    const [courses, setCourses] = useState<CoursesMap>({})
+    const { isLoggedIn, isLoading } = useSession()
+    const router = useRouter()
+    const fetch = useFetch()
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            return
+        }
+
+        fetch('http://mylocal.com:8000/cart')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error()
+                }
+                return res.json()
+            })
+            .then((data: CartType) => {
+                setCart(data)
+
+                const courseFetches = data.items.map((item) => {
+                    return fetch(`http://mylocal.com:8000/courses/${item.course_id}`)
+                        .then((res) => {
+                            if (!res.ok) {
+                                throw new Error()
+                            }
+                            return res.json()
+                        })
+                        .then((course: Course) => {
+                            setCourses((prevCourses) => ({ ...prevCourses, [course.id]: course }))
+                        })
+                })
+
+                Promise.all(courseFetches).catch(() => {
+                    toast.error('Something went wrong')
+                })
+            })
+            .catch(() => {
+                toast.error('Something went wrong')
+            })
+    }, [fetch, isLoggedIn])
+
+    if (isLoading) {
+        return null
+    }
+
+    if (!isLoggedIn) {
+        router.push('/login')
+        return null
+    }
+
+    const handleDeleteItem = async (id: string) => {
+        try {
+            const res = await fetch(`http://mylocal.com:8000/cart/items/${id}`, {
+                method: 'DELETE',
+            })
+
+            if (!res.ok) {
+                throw new Error()
+            }
+
+            if (cart) {
+                setCart({
+                    ...cart,
+                    items: cart.items.filter((item) => item.course_id !== id),
+                })
+            }
+        } catch (err) {
+            toast.error('Something went wrong')
+        }
+    }
+
+    return (
+        <>
+            <Head>
+                <title>Cart</title>
+            </Head>
+            <Layout>
+                <div className="flex w-full flex-col items-center justify-center p-4">
+                    <h1 className="mb-4 text-3xl font-bold">Shopping Cart</h1>
+                    <div className="w-full bg-gray-100 p-4 sm:w-1/2">
+                        {cart &&
+                            cart.items.map((item) => {
+                                const course = courses[item.course_id]
+                                if (!course) return null
+
+                                return (
+                                    <ProductCard
+                                        key={item.course_id}
+                                        product={{
+                                            id: course.id,
+                                            name: course.name,
+                                            image: '',
+                                            price: course.price,
+                                        }}
+                                        onDelete={handleDeleteItem}
+                                    />
+                                )
+                            })}
+                        <button className="mt-4 w-full rounded bg-green-500 p-4 text-white">Checkout</button>
+                    </div>
+                </div>
+            </Layout>
+        </>
+    )
+}
--- frontend/pages/courses/index.tsx
+++ frontend/pages/courses/index.tsx
@@ -3,6 +3,9 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import { useSession } from '@/session/context'
+import { toast } from 'react-hot-toast'
+import { useFetch } from '@/services/fetch'
 
 type Course = {
     id: string
@@ -11,10 +14,48 @@ type Course = {
     image: string
 }
 
-function Card(props: Course) {
+type Cart = {
+    items: CartItem[]
+}
+
+type CartItem = {
+    course_id: string
+}
+
+type CardProps = Course & {
+    isOwned: boolean
+    isInCart: boolean
+    isLoggedIn: boolean
+    onAddToCart: () => void
+}
+
+function Card(props: CardProps) {
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+
+        if (props.isOwned) {
+            window.location.href = `/dashboard/course/${props.id}`
+            return
+        }
+        if (props.isInCart) {
+            // Do nothing.
+            return
+        }
+        if (!props.isLoggedIn) {
+            window.location.href = `/login`
+            return
+        }
+
+        props.onAddToCart()
+    }
+
+    const linkURL: string = props.isOwned
+        ? `/dashboard/course/${encodeURIComponent(props.id)}`
+        : `/courses/${encodeURIComponent(props.id)}`
+
     return (
         <Link
-            href={`/courses/${encodeURIComponent(props.id)}`}
+            href={linkURL}
             className="flex w-1/2 flex-col items-center rounded-lg border border-gray-200 bg-white shadow hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 md:max-w-xl md:flex-row"
         >
             <Image
@@ -28,6 +69,19 @@ function Card(props: Course) {
             <div className="flex flex-col justify-between p-4 leading-normal">
                 <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.name}</h5>
                 <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{props.description}</p>
+                {props.isOwned ? (
+                    <button onClick={handleSubmit} className="w-full rounded bg-blue-500 p-2 font-semibold text-white">
+                        Go to Course
+                    </button>
+                ) : props.isInCart ? (
+                    <button disabled className="w-full rounded bg-gray-500 p-2 font-semibold text-white">
+                        In Cart
+                    </button>
+                ) : (
+                    <button onClick={handleSubmit} className="w-full rounded bg-green-500 p-2 font-semibold text-white">
+                        {props.isLoggedIn ? 'Add to Cart' : 'Login to buy'}
+                    </button>
+                )}
             </div>
         </Link>
     )
@@ -35,16 +89,109 @@ function Card(props: Course) {
 
 export default function Courses() {
     const [courses, setCourses] = useState<Course[]>()
+    const [cartCourses, setCartCourses] = useState<string[]>([])
+    const [isLoadingCart, setIsLoadingCart] = useState<boolean>(true)
+    const [ownedCourses, setOwnedCourses] = useState<string[]>([])
+    const [isLoadingOwned, setIsLoadingOwned] = useState<boolean>(true)
+    const { isLoggedIn, isLoading } = useSession()
+    const fetch = useFetch()
 
     useEffect(() => {
         fetch('http://mylocal.com:8000/courses')
             .then((res) => res.json())
             .then((data) => setCourses(data))
-    }, [])
+    }, [fetch])
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            setCartCourses([])
+            setOwnedCourses([])
+        }
+    }, [isLoggedIn])
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            return
+        }
+        fetch('http://mylocal.com:8000/cart')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error()
+                }
+                return res.json()
+            })
+            .then((data: Cart) => {
+                const incart = data.items.map((item: CartItem) => item.course_id)
+                setCartCourses(incart)
+                setIsLoadingCart(false)
+            })
+            .catch(() => {
+                toast.error('Something went wrong')
+            })
+    }, [fetch, isLoggedIn])
+
+    useEffect(() => {
+        if (!isLoggedIn) {
+            return
+        }
+        fetch('http://mylocal.com:8000/courses/owned')
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error()
+                }
+                return res.json()
+            })
+            .then((data: Course[]) => {
+                const owned = data.map((item: Course) => item.id)
+                setOwnedCourses(owned)
+                setIsLoadingOwned(false)
+            })
+            .catch(() => {
+                toast.error('Something went wrong')
+            })
+    }, [fetch, isLoggedIn])
+
+    const handleAddToCart = (courseID: string) => {
+        fetch('http://mylocal.com:8000/cart/items', {
+            method: 'PUT',
+            body: JSON.stringify({ course_id: courseID }),
+        })
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error()
+                }
+                window.location.href = `/cart`
+            })
+            .catch(() => {
+                toast.error('Something went wrong')
+            })
+    }
+
+    if (isLoading) {
+        return null
+    }
+
+    if (isLoggedIn && (isLoadingCart || isLoadingOwned)) {
+        return null
+    }
 
     if (!courses) {
         return null
     }
+
+    const coursePriority = (course: Course) => {
+        if (ownedCourses.includes(course.id)) {
+            return 2
+        }
+        if (cartCourses.includes(course.id)) {
+            return 1
+        }
+        return 0
+    }
+
+    const sortedCourses = [...courses].sort((a, b) => {
+        return coursePriority(a) - coursePriority(b)
+    })
 
     return (
         <>
@@ -53,8 +200,17 @@ export default function Courses() {
             </Head>
             <Layout>
                 <div className="flex flex-col items-center space-y-5 pt-6 pb-6">
-                    {courses.map((course) => (
-                        <Card {...course} key={course.name} />
+                    {sortedCourses.map((course) => (
+                        <Card
+                            {...course}
+                            isOwned={ownedCourses.includes(course.id)}
+                            isInCart={cartCourses.includes(course.id)}
+                            isLoggedIn={isLoggedIn}
+                            onAddToCart={() => {
+                                handleAddToCart(course.id)
+                            }}
+                            key={course.name}
+                        />
                     ))}
                 </div>
             </Layout>
