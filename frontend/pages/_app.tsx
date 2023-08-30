f6e20c30d8d4e9cecff21a5715ba3217bd2088fb
Paolo Calao <paolo.calao@gmail.com>
2023-08-30 11:34:10 +0200

    Integrate paypal checkout

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,12 +2,21 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { SessionProvider } from '@/session/context'
 import { Toaster } from 'react-hot-toast'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
         <SessionProvider {...pageProps}>
             <Toaster position="bottom-center" />
-            <Component {...pageProps} />
+
+            <PayPalScriptProvider
+                options={{
+                    clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID || '',
+                    currency: 'EUR',
+                }}
+            >
+                <Component {...pageProps} />
+            </PayPalScriptProvider>
         </SessionProvider>
     )
 }
