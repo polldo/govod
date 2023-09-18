59b2a75dd209ba430d8afc2919d83774c6658625
Paolo Calao <paolo.calao@gmail.com>
2023-09-19 00:50:39 +0200

    Draft

--- email/email.go
+++ email/email.go
@@ -1,8 +1,16 @@
 package email
 
 import (
+	"bytes"
+	"embed"
+	_ "embed"
+	"fmt"
+	"html/template"
 	"net/smtp"
 )
+
+//go:embed templates
+var templates embed.FS
 
 type Emailer struct {
 	auth smtp.Auth
@@ -16,7 +24,22 @@ func New(address string, password string, host string, port string) *Emailer {
 }
 
 func (e *Emailer) SendToken(scope string, token string, to string) error {
-	msg := []byte(token)
+	t, err := template.New("email").ParseFS(templates, "templates/reset-password.tmpl")
+	if err != nil {
+		return fmt.Errorf("parsing email template: %w", err)
+	}
+
+	var data struct {
+		Link string
+	}
+	data.Link = "www.google.it"
+
+	var body bytes.Buffer
+	err = t.ExecuteTemplate(&body, "html", data)
+	if err != nil {
+		return fmt.Errorf("executing template: %w", err)
+	}
+
 	dst := []string{to}
-	return smtp.SendMail(e.host, e.auth, e.from, dst, msg)
+	return smtp.SendMail(e.host, e.auth, e.from, dst, body.Bytes())
 }
